00b47e9d6f5804298bbb1a1eec8d68fb7ba6992f
Will Xyen <none@none>
2015-09-19 18:14:05 -0400

    More github auth stuff

--- html/js/firebase_helpers.js
+++ html/js/firebase_helpers.js
@@ -1,33 +1,42 @@
 function getTagID(value, callback) {
 	var ref = new Firebase("https://team-hack.firebaseio.com/tags");
 
-	ref.orderByValue().equalTo(value).once("value", function(snapshot) {
-		callback(snapshot.val());
-	});
+	ref.orderByValue().equalTo(value).once("value", function(id){callback(id)});
 }
 
 function addTag(value, callback) {
 	var lookup = new Firebase("https://team-hack.firebaseio.com/tags/");
-	lookup.orderByKey().limitToLast(1).once("value", function(snapshot) {
-		snapshot.forEach(function(childSnapshot){
-			var ref = new Firebase("https://team-hack.firebaseio.com/tags/" + (parseInt(childSnapshot.key()) + 1));
-			ref.set(value);
-			callback(parseInt(childSnapshot.key()) + 1);
-		})
-	});
+	ref.push(value);
+	getTagID(value, callback);
 }
 
 function getOrAddTag(value, callback){
-	if (getTagID(value)){
-		return getTagID(value, callback);
-	}else{
-		return addTag(value, callback);
-	}
+	getTagID(value, function(id){
+		if (id){
+			callback(id);
+		}else{
+			addTag(value, callback)
+		}
+	});
 }
 
 function autoCompleteTag(value, callback) {
 	var ref = new Firebase("https://team-hack.firebaseio.com/tags");
-	ref.orderByValue().startAt(value).endAt(value + "z").once("value", function(snapshot) {
+	ref.orderByValue().startAt(value).endAt(value + "~").once("value", function(snapshot) {
 		callback(snapshot.val());
 	});
+}
+
+function updateUserLogin(authData){
+	var user = new Firebase("https://team-hack.firebaseio.com/user/" + authData.uid);
+	user.update({"image-link" : authData.github.profileImageURL});
+	user.once("value", function(snapshot){
+		if (!snapshot.child("email").exists()){
+			user.update({"email" : authData.github.email})
+		}
+		if (!snapshot.child("name").exists()){
+			user.update({"name" : authData.github.displayName})
+		}
+	});
+	// console.log("Authenticated successfully with payload:", authData);
 }
